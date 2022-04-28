69eb5873a067f91e5c50ba84ac18c632d73a254d
ahlem11 <nad_ahlem@yahoo.com>
2022-04-27 15:35:14 -1100

    auth & tran

--- resources/views/auth/two-factor-challenge.blade.php
+++ resources/views/auth/two-factor-challenge.blade.php
@@ -1,57 +1,60 @@
-<x-guest-layout>
+<x-layouts.app>
     <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+        <div class="card border-0 mb-0">
+            <div class="py-4 card-body">
+                <div x-data="{ recovery: false }">
+                    <div class="mb-4 text-base text-gray-600" x-show="! recovery" >
+                        <p class="tracking-[.25em]">{{ __('Please confirm access to your account by entering the authentication code provided by your authenticator application.') }}
+                        </p></div>
 
-        <div x-data="{ recovery: false }">
-            <div class="mb-4 text-sm text-gray-600" x-show="! recovery">
-                {{ __('Please confirm access to your account by entering the authentication code provided by your authenticator application.') }}
+                    <div class="mb-4 text-base text-gray-600" x-show="recovery">
+                        {{ __('Please confirm access to your account by entering one of your emergency recovery codes.') }}
+                    </div>
+
+                    <x-jet-validation-errors class="mb-4" />
+
+                    <form method="POST" action="{{ route('two-factor.login') }}">
+                        @csrf
+
+                        <div class="mt-4" x-show="! recovery">
+                            <x-jet-input id="code" class="block mt-1 w-full" type="text" placeholder="Code" inputmode="numeric" name="code" autofocus x-ref="code" autocomplete="one-time-code" />
+                        </div>
+
+                        <div class="mt-4" x-show="recovery">
+                            <x-jet-input id="recovery_code" class="block mt-1 w-full" placeholder="Recovery code"  type="text" name="recovery_code" x-ref="recovery_code" autocomplete="one-time-code" />
+                        </div>
+
+                        <div class="flex items-center justify-end mt-2">
+                            <x-jet-button>
+                                {{ __('Log in') }}
+                            </x-jet-button>
+                        </div>
+                        <div class="flex items-center justify-center mt-2">
+                            <button type="button" class="text-sm text-gray-600 hover:text-gray-900 underline cursor-pointer"
+                                            x-show="! recovery"
+                                            x-on:click="
+                                                recovery = true;
+                                                $nextTick(() => { $refs.recovery_code.focus() })
+                                            ">
+                                {{ __('Use a recovery code') }}
+                            </button>
+
+                            <button type="button" class="text-sm text-gray-600 hover:text-gray-900 underline cursor-pointer"
+                                            x-show="recovery"
+                                            x-on:click="
+                                                recovery = false;
+                                                $nextTick(() => { $refs.code.focus() })
+                                            ">
+                                {{ __('Use an authentication code') }}
+                            </button> 
+                        </div>
+                    </form>
+                </div>
             </div>
-
-            <div class="mb-4 text-sm text-gray-600" x-show="recovery">
-                {{ __('Please confirm access to your account by entering one of your emergency recovery codes.') }}
-            </div>
-
-            <x-jet-validation-errors class="mb-4" />
-
-            <form method="POST" action="{{ route('two-factor.login') }}">
-                @csrf
-
-                <div class="mt-4" x-show="! recovery">
-                    <x-jet-label for="code" value="{{ __('Code') }}" />
-                    <x-jet-input id="code" class="block mt-1 w-full" type="text" inputmode="numeric" name="code" autofocus x-ref="code" autocomplete="one-time-code" />
-                </div>
-
-                <div class="mt-4" x-show="recovery">
-                    <x-jet-label for="recovery_code" value="{{ __('Recovery Code') }}" />
-                    <x-jet-input id="recovery_code" class="block mt-1 w-full" type="text" name="recovery_code" x-ref="recovery_code" autocomplete="one-time-code" />
-                </div>
-
-                <div class="flex items-center justify-end mt-4">
-                    <button type="button" class="text-sm text-gray-600 hover:text-gray-900 underline cursor-pointer"
-                                    x-show="! recovery"
-                                    x-on:click="
-                                        recovery = true;
-                                        $nextTick(() => { $refs.recovery_code.focus() })
-                                    ">
-                        {{ __('Use a recovery code') }}
-                    </button>
-
-                    <button type="button" class="text-sm text-gray-600 hover:text-gray-900 underline cursor-pointer"
-                                    x-show="recovery"
-                                    x-on:click="
-                                        recovery = false;
-                                        $nextTick(() => { $refs.code.focus() })
-                                    ">
-                        {{ __('Use an authentication code') }}
-                    </button>
-
-                    <x-jet-button class="ml-4">
-                        {{ __('Log in') }}
-                    </x-jet-button>
-                </div>
-            </form>
         </div>
     </x-jet-authentication-card>
-</x-guest-layout>
+</x-layouts.app>
+
+
+
+
--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -2,7 +2,7 @@
     <div class="page-header min-vh-100" style="background-image: url('{{asset('assets/images/19333414.webp')}}');">
         <span class="mask bg-gradient-dark opacity-6"></span>
         <div class="container">
-            <div class="w-full sm:max-w-md mt-6 bg-white shadow-md overflow-hidden sm:rounded-lg mx-auto">
+            <div class="w-full sm:max-w-md mx-6 bg-white shadow-md overflow-hidden sm:rounded-lg mx-auto">
                 {{ $slot }}
             </div>
         </div>
